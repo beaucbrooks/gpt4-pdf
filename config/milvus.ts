7cd4a3f94363d2b36f4eb2b148c12ed00b3cb0e1
Beau Brooks <11509593+beaucbrooks@users.noreply.github.com>
2023-04-16 09:33:12 +0000

    remove pinecone db

--- config/milvus.ts
+++ config/milvus.ts
@@ -1,6 +1,3 @@
-/**
- * Change the namespace to the namespace on Pinecone you'd like to store your embeddings.
- */
 
 if (!process.env.MILVUS_URL) {
     throw new Error('Missing MILVUS_URL in .env file');
